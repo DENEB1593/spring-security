5c52f8383a67b95e4d1b44513ebffcbf454470e0
deneb1593 <deneb1593@gmail.com>
2022-11-19 00:07:18 +0900

    security 설정 방식 변경

--- src/main/java/com/example/springsecurity/security/SecurityConfigure.java
+++ src/main/java/com/example/springsecurity/security/SecurityConfigure.java
@@ -1,15 +1,31 @@
 package com.example.springsecurity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfigure extends WebSecurityConfigurerAdapter {
-  @Override
-  public void configure(WebSecurity web) throws Exception {
-    web.ignoring().antMatchers("/h2-console/**");
+public class SecurityConfigure {
+
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return web -> {
+      web.ignoring().antMatchers("/h2-console/**");
+    };
   }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf().disable();
+    http.cors().disable();
+
+    return http.build();
+  }
+
 }
